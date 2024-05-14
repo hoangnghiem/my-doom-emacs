4cefe8ab1eec0d363e53bdbd1108705a0e96242f
Hoang Nghiem <hoangnghiem1711@gmail.com>
2024-05-14 11:57:11 +0700

    Use literate config

---
+++ packages.el
@@ -0,0 +1,110 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
+
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+                                        ;(package! some-package)
+
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; https://github.com/raxod502/straight.el#the-recipe-format
+                                        ;(package! another-package
+                                        ;  :recipe (:host github :repo "username/repo"))
+
+;; If the package you are trying to install does not contain a PACKAGENAME.el
+;; file, or is located in a subdirectory of the repo, you'll need to specify
+;; `:files' in the `:recipe':
+                                        ;(package! this-package
+                                        ;  :recipe (:host github :repo "username/repo"
+                                        ;           :files ("some-file.el" "src/lisp/*.el")))
+
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
+                                        ;(package! builtin-package :disable t)
+
+;; You can override the recipe of a built in package without having to specify
+;; all the properties for `:recipe'. These will inherit the rest of its recipe
+;; from Doom or MELPA/ELPA/Emacsmirror:
+                                        ;(package! builtin-package :recipe (:nonrecursive t))
+                                        ;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+
+;; Specify a `:branch' to install a package from a particular branch or tag.
+;; This is required for some packages whose default branch isn't 'master' (which
+;; our package manager can't deal with; see raxod502/straight.el#279)
+                                        ;(package! builtin-package :recipe (:branch "develop"))
+
+;; Use `:pin' to specify a particular commit to install.
+                                        ;(package! builtin-package :pin "1a2b3c4d5e")
+
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+                                        ;(unpin! pinned-package)
+;; ...or multiple packages
+                                        ;(unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+                                        ;(unpin! t)
+
+(package! aggressive-indent)
+(package! wakatime-mode)
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
+(package! eink-theme :recipe (:host github :repo "maio/eink-emacs"))
+(package! string-inflection)
+(package! expand-region)
+;; (package! company-tabnine :recipe (:host github :repo "karta0807913/company-tabnine"))
+(package! evil-matchit)
+(package! lsp-origami)
+(package! prettier-js)
+(package! spacemacs-theme)
+(package! humanoid-themes)
+(package! apropospriate-theme)
+(package! nord-theme)
+(package! one-themes)
+(package! modus-themes)
+(package! anti-zenburn-theme :recipe (:host github :repo "m00natic/anti-zenburn-theme"))
+(package! bespoke-themes :recipe (:host github :repo "mclear-tools/bespoke-themes"))
+(package! afternoon-theme)
+(package! auto-dim-other-buffers)
+(package! visual-regexp)
+(package! visual-regexp-steroids)
+(package! rails-routes :pin "eab995a9297ca5bd9bd4f4c2737f2fecfc36def0")
+(package! rails-i18n :pin "8e87e4e48e31902b8259ded28a208c2e7efea6e9")
+(package! multi-line :pin "625c608443f98bb34b4d5600d52c198509fb64d0")
+(package! ruby-refactor :pin "e6b7125878a08518bffec6942df0c606f748e9ee")
+(package! fold-this)
+;; (package! apheleia)
+(package! yafolding)
+;; (package! cape-yasnippet :recipe (:host github :repo "elken/cape-yasnippet"))
+(package! ox-tailwind :recipe (:host github :repo "vascoferreira25/ox-tailwind"))
+(package! all-the-icons)
+(package! expreg)
+(package! dirvish)
+(package! move-text)
+(package! kaolin-themes)
+(package! nano-theme :recipe (:host github :repo "rougier/nano-theme"))
+
+;; (unpin! multiple-cursors)
+(unpin! lsp-mode)
+(unpin! nerd-icons)
+(unpin! apheleia)
+;; (unpin! treemacs)
+;; (unpin! company-box)
+
+(package! transient
+  :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+  :recipe (:host github :repo "magit/transient"))
+
+(package! with-editor
+  :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+  :recipe (:host github :repo "magit/with-editor"))
+
+(package! codeium :recipe (:host github :repo "Exafunction/codeium.el"))
+(package! toc-mode)
+
+;; (package! tabnine-capf :recipe (:host github :repo "50ways2sayhard/tabnine-capf"))
+(package! svg-lib)
+;; (package! treemacs-nerd-icons)
